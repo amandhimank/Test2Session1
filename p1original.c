000441b4383eb370a470173e832408832c5f9e30
amandhimank <96230132+amandhimank@users.noreply.github.com>
2022-02-23 04:46:06 +0000

    1st program

--- p1original.c
+++ p1original.c
@@ -7,21 +7,21 @@ void input(float *x1, float *y1, float *x2, float *y2)
   scanf("%f %f %f %f", x1, y1, x2, y2);
 }
 
-void find_distance(float x1, float y1, float x2, float y2, float *area)
+void find_distance(float x1, float y1, float x2, float y2, float *distance)
 {
-  *area = sqrt(((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1)));
+  *distance = sqrt(((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1)));
 }
 
-void output(float x1, float y1,float x2, float y2, float area)
+void output(float x1, float y1,float x2, float y2, float distance)
 {
-  printf("The distance between points (%f, %f) and (%f, %f) is %f\n", x1, y1, x2, y2, area);
+  printf("The distance between points (%f, %f) and (%f, %f) is %f\n", x1, y1, x2, y2, distance);
 }
 
 int main()
 {
-  float x1, y1, x2, y2, area;
+  float x1, y1, x2, y2, distance;
   input(&x1, &y1, &x2, &y2);
-  find_distance(x1, y1, x2, y2, &area);
-  output(x1, y1, x2, y2, area);
+  find_distance(x1, y1, x2, y2, &distance);
+  output(x1, y1, x2, y2, distance);
   return 0; 
 }
---
+++ p2original.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3);
+int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3);
+void output(float x1, float y1, float x2, float y2,float x3, float y3, int istriangle);
+
+int main()
+{
+  float x1, y1, x2, y2;
+}
+
+
